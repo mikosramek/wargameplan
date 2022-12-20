2922aa73d6b10a1960d3bdf51d4b14305ccd162c
mikosramek <miko@mikosramek.ca>
2022-12-20 09:36:38 -0500

    feat: nextjs setup

---
+++ packages/react-frontend/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Warhammer Gameplan</title>
+        <meta name="description" content="TODO" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Warhammer Gameplan</h1>
+      </main>
+    </>
+  );
+}
