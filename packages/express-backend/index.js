dc0cf8b3b66417a4bd2ddb6ba320e92e59478e72
mikosramek <miko@mikosramek.ca>
2023-02-12 01:06:28 -0500

    feat: another certbot attempt

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -1,8 +1,33 @@
 require("dotenv").config();
+const fs = require("fs");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const https = require("https");
+
+// Certificate
+// const privateKey = fs.readFileSync(
+//   "/etc/letsencrypt/live/api.wargameplanner.com/privkey.pem",
+//   "utf8"
+// );
+// const certificate = fs.readFileSync(
+//   "/etc/letsencrypt/live/api.wargameplanner.com/cert.pem",
+//   "utf8"
+// );
+// const ca = fs.readFileSync(
+//   "/etc/letsencrypt/live/api.wargameplanner.com/chain.pem",
+//   "utf8"
+// );
+
+// /etc/letsencrypt/live/api.wargameplanner.com/fullchain.pem
+// /etc/letsencrypt/live/api.wargameplanner.com/privkey.pem
+
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca,
+// };
 
 const { DB_NAME, DB_USER, DB_PASSWORD, FRONT_END_URL, DEV_FRONT_END_URL } =
   process.env;
@@ -43,12 +68,12 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get(
-  "/.well-known/acme-challenge/BYicdxSWYOGe89NpOFV4KsKcbuo4K3fgfqpgwZfGkjU",
+  "/.well-known/acme-challenge/4BH_FsJIlT_4ZnJp5YNehlkUdzYJ7LUZAoIkd_JKTPI",
   (req, res) => {
     res
       .status(200)
       .send(
-        "BYicdxSWYOGe89NpOFV4KsKcbuo4K3fgfqpgwZfGkjU.L8hOzbQBwnMUT6BqD8rvjfuRPc5j9c09WovBAG4KW5A"
+        "4BH_FsJIlT_4ZnJp5YNehlkUdzYJ7LUZAoIkd_JKTPI.iD-pIIdGxl50WjXWvKKZUW_LzEzynXY4HjcjIVOTbB4"
       );
   }
 );
@@ -62,3 +87,9 @@ app.use("/api/v1/steps", require("./steps/routes"));
 app.use("/api/v1/rules", require("./steps/rulesRoutes"));
 
 app.listen(1337);
+
+// const httpsServer = https.createServer(credentials, app);
+
+// httpsServer.listen(1337, () => {
+//   console.log("HTTPS Server running on port 1337");
+// });
